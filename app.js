d44aecee1be446a27ccecb4f037371fe9d4fd506
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-02-21 13:11:44 -0300

    GraphQL ApiTeste Versao 1.1 GraphQL

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+//const { graphqlHTTP } = require('express-graphql'); Outra opção
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]
+        }
+
+        type RootMutation{
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Estudar', 'Navegar', 'Programar', "Reunião"];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
