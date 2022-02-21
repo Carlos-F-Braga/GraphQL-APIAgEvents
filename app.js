aaa0b4fadcd61ad05932910582e78105832a441b
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-02-21 14:01:51 -0300

    GraphQL ApiTeste Versao 1.2 createEvent Mutation and events Query completed

--- app.js
+++ app.js
@@ -6,16 +6,34 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Event{
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput{
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+
         type RootQuery {
-            events: [String!]
+            events: [Event!]
         }
 
         type RootMutation{
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -25,11 +43,19 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return ['Estudar', 'Navegar', 'Programar', "Reunião"];
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            };
+            console.log(event);
+            events.push(event);
+            return event;
         }
     },
     graphiql: true
