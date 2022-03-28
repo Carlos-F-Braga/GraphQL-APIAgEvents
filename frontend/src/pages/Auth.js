d714d3a0537dbf9eddf0cf975667569169f5c9a3
Carlos <carlos.goncalves@agrofauna.com.br>
2022-03-28 13:47:43 -0300

    React Auth.css Style Finished

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 
+import './Auth.css';
+
 class AuthPage extends Component{
     render() {
-        return (<h1>The Auth Page</h1>);
+        return (
+        <form className="auth-form"> 
+            <div className="form-control">
+                <label hemlFor="email">E-mail</label>
+                <input type="email" id="email" />
+            </div>
+            <div className="form-control">
+                <label hemlFor="password">Password</label>
+                <input type="password" id="password" />
+            </div>
+            <div className="form-actions">
+                <button type="submit">Submit</button>
+                <button type="button">Switch to Signup</button>
+            </div>
+        </form>
+        )
     }
 }
 
