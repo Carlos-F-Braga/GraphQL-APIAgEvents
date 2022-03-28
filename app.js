76f75302e6dc7d4240294d130b9fc5a44dbc539b
Carlos <carlos.goncalves@agrofauna.com.br>
2022-03-28 14:28:36 -0300

    Frontend Backend Comunication Finished completamente Programada Login e  Signup states

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use ((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS'){
+        return res.sendStatus(200);
+    }
+    next();
+})
+
 app.use(isAuth);
 
 app.use('/graphql', graphqlHttp({
@@ -26,7 +36,7 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${
     process.env.MONGO_PASSWORD
 }@cluster0.38xfe.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 ).then(() => {
-    app.listen(3000);
+    app.listen(8000);
     console.log("Funcionando Corretamente - Conexão Estabelecida com Sucesso");
 }).catch(err => {
     console.log(err);
