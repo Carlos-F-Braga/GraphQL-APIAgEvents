b1da3541643962837f321efc0b9ea1070944d385
Carlos <carlos.goncalves@agrofauna.com.br>
2022-06-09 14:13:41 -0300

    Styled Components installed

---
+++ frontend/src/components/Backdrop/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Backdrop = styled.div` 
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.83); 
+
+`
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-<head><link rel="icon" href="https://www.flaticon.com/premium-icon/testing_1354217" /> </head>
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
