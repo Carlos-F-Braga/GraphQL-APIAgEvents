f426e63a03fd3cdae2c72c5effc75f8600ddf205
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-17 18:26:11 -0300

    putting production api

--- frontend/src/context/api-context.js
+++ frontend/src/context/api-context.js
@@ -1,6 +1,7 @@
 import React, { createContext } from "react"
 
-export const ApiContext = createContext('http://localhost:8000/graphql')
+// export const ApiContext = createContext('http://localhost:8000/graphql')
+export const ApiContext = createContext('http://app-agevents.herokuapp.com/graphql')
 export const ApiProvider = ({ children }) => {
 
   return <ApiContext.Provider value={{ }}>
